e90a50092ce7d3cf92cbbe221d4da44d2e8ef960
Luís Ferreira <luisfferreira.dev@gmail.com>
2026-04-07 17:05:49 +0100

    Merge branch 'feature/design-system' (#15)

---
+++ src/lib/components/Container.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	interface Props {
+		size?: 'sm' | 'default' | 'lg';
+		class?: string;
+		children?: any;
+	}
+
+	let { size = 'default', class: className = '', children }: Props = $props();
+
+	const maxWidths = {
+		sm: 'max-w-[1080px]',
+		default: 'max-w-[1280px]',
+		lg: 'max-w-[1440px]'
+	};
+
+	const maxWidth = $derived(maxWidths[size]);
+</script>
+
+<div class="mx-auto w-full {maxWidth} {className}">
+	{@render children?.()}
+</div>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,3 @@
 // place files you want to import through the `$lib` alias in this folder.
+export { themeStore } from './theme-store';
+export { default as Container } from './components/Container.svelte';
